4f2a96956331c864a728010f5a870f214a75df2c
Evgenij-gostev <evgenij-gostev@yandex.ru>
2018-10-27 11:23:00 +0300

    Initial Commit

---
+++ Task_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task_1
+//
+//  Created by Евгений Гостев on 27.10.2018.
+//  Copyright © 2018 Evgenij Gostev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task_1
+//
+//  Created by Евгений Гостев on 27.10.2018.
+//  Copyright © 2018 Evgenij Gostev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
